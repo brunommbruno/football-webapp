8023184b4c0511064e1a9b475f2dd9db009fde11
brunommbruno <brunommbruno@gmail.com>
2021-01-22 01:45:12 +0000

    added loop through api return

--- src/Components/Fixtures.js
+++ src/Components/Fixtures.js
@@ -1,3 +1,4 @@
+import { List } from "@material-ui/core";
 import { Component } from "react";
 
 import { ListGroup, Col, Row } from "react-bootstrap";
@@ -6,33 +7,79 @@ class Fixtures extends Component{
 
     render(){
 
-        let fixtures = this.props.fixtures;
+        const fixtures = this.props.fixtures;
+
+        const leagues = [
+            {
+                league_name: "Premier League", 
+                fixtures: fixtures.filter(fixture => fixture.league_id == 2790),
+                flag: "https://media.api-sports.io/flags/gb.svg",
+                logo: "https://media.api-sports.io/football/leagues/39.png",
+            },
+            {
+                league_name: "Primera Division",
+                fixtures: fixtures.filter(fixture => fixture.league_id == 2833),
+                flag: "https://media.api-sports.io/flags/es.svg",
+                logo: "https://media.api-sports.io/football/leagues/140.png",
+            },
+            {
+                league_name: "Ligue 1",
+                fixtures: fixtures.filter(fixture => fixture.league_id == 2664),
+                flag: "https://media.api-sports.io/flags/fr.svg",
+                logo: "https://media.api-sports.io/football/leagues/61.png",
+            },
+            {
+                league_name: "Serie A",
+                fixtures: fixtures.filter(fixture => fixture.league_id == 2857),
+                flag: "https://media.api-sports.io/flags/it.svg",
+                logo: "https://media.api-sports.io/football/leagues/135.png",
+            },
+            {
+                league_name: "Bundesliga",
+                fixtures: fixtures.filter(fixture => fixture.league_id == 2755),
+                flag: "https://media.api-sports.io/flags/de.svg",
+                logo: "https://media.api-sports.io/football/leagues/78.png",
+            },
+
+
+        ]
 
         return(
             <div className="fixtures">
-                <ListGroup>
-                    {fixtures.map((fixture) => (
-                        <ListGroup.Item>
-                            <Row>
-                                <Col className="col-1">
-                                <p className="status">{fixture.statusShort}</p>
-                                </Col>
-                                <Col className="text-center">
-                                <p>{fixture.homeTeam.team_name}</p>
-                                <img src={fixture.homeTeam.logo}/>
-                                </Col>
-                                <Col className="col-2">
-                                <p className="score">{fixture.goalsHomeTeam} : {fixture.goalsAwayTeam}</p>
-                                </Col>
-                                <Col className="text-center">
-                                <p>{fixture.awayTeam.team_name}</p>
-                                <img src={fixture.awayTeam.logo}/>
-                                </Col>
+
+                {leagues.map((league) => (
+                    <ListGroup className="league-group">
+                        <ListGroup.Item style={{paddingTop: "0.3rem"}}>
+                            <Row className="league-title">
+                                <img style={{height: "1rem"}}src={league.logo} />
+                                <p> - {league.league_name}</p>
                             </Row>
                         </ListGroup.Item>
-                    ))}
+                        {league.fixtures.map((fixture) => (
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col className="col-1">
+                                    <p className="status">{fixture.statusShort}</p>
+                                    </Col>
+                                    <Col className="text-center col-4">
+                                    <p>{fixture.homeTeam.team_name}</p>
+                                    <img src={fixture.homeTeam.logo}/>
+                                    </Col>
+                                    <Col className="col-2">
+                                    <p className="score">{fixture.goalsHomeTeam != null ? fixture.goalsHomeTeam :"?"} : {fixture.goalsAwayTeam != null ? fixture.goalsAwayTeam :"?"}</p>
+                                    </Col>
+                                    <Col className="text-center col-4">
+                                    <p>{fixture.awayTeam.team_name}</p>
+                                    <img src={fixture.awayTeam.logo}/>
+                                    </Col>
+                                    <Col className="col-1">
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                ))}
 
-                </ListGroup>
             </div>
         )
     }
