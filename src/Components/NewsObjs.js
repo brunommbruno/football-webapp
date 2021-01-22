156c1b8625b33c6dc1e2bba6c48f06be88b68c57
brunommbruno <brunommbruno@gmail.com>
2021-01-22 15:39:43 +0000

    created component with sample news articles

---
+++ src/Components/NewsObjs.js
@@ -0,0 +1,106 @@
+export const NewsObjs = [
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Soccer-Diego Costa rescinds contract with Atletico - club statement - Reuters UK",
+            "description": "Atletico Madrid have agreed to rescind striker Diego Costa's contract, the La Liga leaders said on Tuesday.",
+            "url": "https://uk.reuters.com/article/uk-soccer-spain-atm-get-preview-idUKKBN293128",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20201229&t=2&i=1546046817&r=LYNXMPEGBS0JX&w=800",
+            "publishedAt": "2020-12-29T12:19:00Z",
+            "content": "By Reuters Staff\r\nFILE PHOTO: Soccer Football - La Liga Santander - Atletico Madrid v Granada - Wanda Metropolitano, Madrid, Spain - September 27, 2020. Atletico Madrid's Diego Costa celebrates scori… [+391 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Soccer-Diego Costa rescinds contract with Atletico - club statement - Reuters UK",
+            "description": "Atletico Madrid have agreed to rescind striker Diego Costa's contract, the La Liga leaders said on Tuesday.",
+            "url": "https://in.reuters.com/article/uk-soccer-spain-atm-get-preview-idUKKBN293128",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20201229&t=2&i=1546046817&r=LYNXMPEGBS0JX&w=800",
+            "publishedAt": "2020-12-29T12:19:00Z",
+            "content": "By Reuters Staff\r\nFILE PHOTO: Soccer Football - La Liga Santander - Atletico Madrid v Granada - Wanda Metropolitano, Madrid, Spain - September 27, 2020. Atletico Madrid's Diego Costa celebrates scori… [+391 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "La Liga talking points - Reuters UK",
+            "description": "Talking points from the weekend in Spanish football.",
+            "url": "https://www.reuters.com/article/uk-soccer-spain-idUKKBN29902N",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20210104&t=2&i=1546488435&r=LYNXMPEH0300X&w=800",
+            "publishedAt": "2021-01-04T01:01:00Z",
+            "content": "By Reuters Staff\r\nMADRID (Reuters) - Talking points from the weekend in Spanish football.\r\nFILE PHOTO: Soccer Football - La Liga Santander - Real Madrid v Celta Vigo - Estadio Alfredo Di Stefano, Mad… [+2532 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Athletic Bilbao sack coach Garitano hours after beating Elche - Reuters India",
+            "description": "La Liga side Athletic Bilbao said on Sunday that they have sacked coach Gaizka Garitano, only hours after their 1-0 victory at home over Elche.",
+            "url": "https://www.reuters.com/article/soccer-spain-atb-garitano-idINKBN2980HV",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20210103&t=2&i=1546468980&r=LYNXMPEH0209B&w=800",
+            "publishedAt": "2021-01-03T17:21:00Z",
+            "content": "By Reuters Staff\r\nSoccer Football - La Liga Santander - Real Madrid v Athletic Bilbao - Estadio Alfredo Di Stefano, Madrid, Spain - December 15, 2020 Athletic Bilbao coach Gaizka Garitano REUTERS/Ser… [+497 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Athletic Bilbao sack coach Garitano hours after beating Elche - Reuters India",
+            "description": "La Liga side Athletic Bilbao said on Sunday that they have sacked coach Gaizka Garitano, only hours after their 1-0 victory at home over Elche.",
+            "url": "https://in.reuters.com/article/soccer-spain-atb-garitano-idINKBN2980HV",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20210103&t=2&i=1546468980&r=LYNXMPEH0209B&w=800",
+            "publishedAt": "2021-01-03T17:21:00Z",
+            "content": "By Reuters Staff\r\nSoccer Football - La Liga Santander - Real Madrid v Athletic Bilbao - Estadio Alfredo Di Stefano, Madrid, Spain - December 15, 2020 Athletic Bilbao coach Gaizka Garitano REUTERS/Ser… [+497 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Messi breaks Pele's record goal haul by scoring 644th for Barca - Reuters UK",
+            "description": "Lionel Messi became the all-time top scorer for a single soccer club on Tuesday after scoring his 644th goal for Barcelona during their La Liga match against Real Valladolid, surpassing Pele's record goal haul for Brazilian side Santos.",
+            "url": "https://uk.reuters.com/article/uk-soccer-spain-rev-fcb-idUKKBN28W2OF",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20201222&t=2&i=1545491546&r=LYNXMPEGBL1JZ&w=800",
+            "publishedAt": "2020-12-22T22:40:00Z",
+            "content": "By Reuters Staff\r\nSoccer Football - La Liga Santander - Real Valladolid v FC Barcelona - Estadio Jose Zorrilla, Valladolid, Spain - December 22, 2020 Barcelona's Lionel Messi celebrates scoring their… [+505 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Soccer-Costa misses Atletico training, considering future - Reuters UK",
+            "description": "Atletico Madrid striker Diego Costa missed training on Monday because he is considering his future with the La Liga leaders, a club source said.",
+            "url": "https://uk.reuters.com/article/uk-soccer-spain-atm-costa-idUKKBN2921EV",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20201228&t=2&i=1545972105&r=LYNXMPEGBR0OX&w=800",
+            "publishedAt": "2020-12-28T15:43:00Z",
+            "content": "By Reuters Staff\r\nFILE PHOTO: Soccer Football - La Liga Santander - Atletico Madrid v Elche - Wanda Metropolitano, Madrid, Spain - December 19, 2020 Atletico Madrid's Diego Costa celebrates scoring t… [+1094 chars]"
+        },
+        {
+            "source": {
+                "id": "reuters",
+                "name": "Reuters"
+            },
+            "author": "Reuters Staff",
+            "title": "Soccer-Real Madrid frustrated by Elche to leave Atleti clear at top - Reuters UK",
+            "description": "Real Madrid were held to a disappointing 1-1 draw at struggling Elche on Wednesday, losing ground in the La Liga title race to leaders Atletico Madrid.",
+            "url": "https://uk.reuters.com/article/uk-soccer-spain-elc-mad-report-idUKKBN2942EI",
+            "urlToImage": "https://static.reuters.com/resources/r/?m=02&d=20201230&t=2&i=1546212667&r=LYNXMPEGBT1DK&w=800",
+            "publishedAt": "2020-12-30T22:30:00Z",
+            "content": "By Reuters Staff\r\nSoccer Football - La Liga Santander - Elche v Real Madrid - Estadio Manuel Martinez Valero, Elche, Spain - December 30, 2020 Real Madrid's Karim Benzema shoots at goal REUTERS/Juan … [+815 chars]"
+        },
+]
