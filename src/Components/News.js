4b0f80511b212899155ae7665b4126517c59524d
brunommbruno <brunommbruno@gmail.com>
2021-01-22 15:25:55 +0000

    added app bar and sample news card to news component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,11 +1,34 @@
 import { Component } from "react";
-
+import { Card, Button } from "react-bootstrap";
+import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 class News extends Component{
 
     render(){
         return(
             <div className="news">
-                asd
+                <AppBar position="static" className="appbar">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6">
+                    News
+                    </Typography>
+                </Toolbar>
+                </AppBar>
+
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
             </div>
         )
     }
