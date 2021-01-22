5bfdf280427fc9468da8818213bdbf675987a1bd
brunommbruno <brunommbruno@gmail.com>
2021-01-22 14:21:48 +0000

    added component that stores leagues that will be filtered to use

--- src/Components/HomeFixtures.js
+++ src/Components/HomeFixtures.js
@@ -1,5 +1,6 @@
 import { List } from "@material-ui/core";
 import { Component } from "react";
+import  {LeagueObjs} from "./LeagueObjs";
 
 import { ListGroup, Col, Row, Badge } from "react-bootstrap";
 
@@ -9,43 +10,16 @@ class HomeFixtures extends Component{
 
         const fixtures = this.props.fixtures;
 
-        const leagues = [
-            {
-                league_name: "Premier League", 
-                fixtures: fixtures.filter(fixture => fixture.league_id == 2790),
-                flag: "https://media.api-sports.io/flags/gb.svg",
-                logo: "https://media.api-sports.io/football/leagues/39.png",
-            },
-            {
-                league_name: "Primera Division",
-                fixtures: fixtures.filter(fixture => fixture.league_id == 2833),
-                flag: "https://media.api-sports.io/flags/es.svg",
-                logo: "https://media.api-sports.io/football/leagues/140.png",
-            },
-            {
-                league_name: "Ligue 1",
-                fixtures: fixtures.filter(fixture => fixture.league_id == 2664),
-                flag: "https://media.api-sports.io/flags/fr.svg",
-                logo: "https://media.api-sports.io/football/leagues/61.png",
-            },
-            {
-                league_name: "Serie A",
-                fixtures: fixtures.filter(fixture => fixture.league_id == 2857),
-                flag: "https://media.api-sports.io/flags/it.svg",
-                logo: "https://media.api-sports.io/football/leagues/135.png",
-            },
-            {
-                league_name: "Bundesliga",
-                fixtures: fixtures.filter(fixture => fixture.league_id == 2755),
-                flag: "https://media.api-sports.io/flags/de.svg",
-                logo: "https://media.api-sports.io/football/leagues/78.png",
-            },
-        ]
+        LeagueObjs.forEach(league => {
+            let fixtures_filtered = fixtures.filter(fixture => fixture.league_id == league.league_id);
+
+            league.fixtures = fixtures_filtered;
+        });
 
         return(
             <div className="fixtures">
 
-                {leagues.map((league) => (
+                {LeagueObjs.map((league) => (
                     //if league has no features it does not go through the loop
                     (league.fixtures[0] ? 
                     <ListGroup className="league-group">
@@ -91,7 +65,7 @@ class HomeFixtures extends Component{
                             </ListGroup.Item>
                         ))}
                     </ListGroup> : null)
-                ))}
+                ))}  
 
             </div>
         )
---
+++ src/Components/LeagueObjs.js
@@ -0,0 +1,37 @@
+export const LeagueObjs = [
+        {
+            league_name: "Premier League", 
+            league_id: 2790,
+            flag: "https://media.api-sports.io/flags/gb.svg",
+            logo: "https://media.api-sports.io/football/leagues/39.png",
+            fixtures: [],
+        },
+        {
+            league_name: "Primera Division",
+            league_id: 2833,
+            flag: "https://media.api-sports.io/flags/es.svg",
+            logo: "https://media.api-sports.io/football/leagues/140.png",
+            fixtures: [],
+        },
+        {
+            league_name: "Ligue 1",
+            league_id: 2664,
+            flag: "https://media.api-sports.io/flags/fr.svg",
+            logo: "https://media.api-sports.io/football/leagues/61.png",
+            fixtures: [],
+        },
+        {
+            league_name: "Serie A",
+            league_id: 2857,
+            flag: "https://media.api-sports.io/flags/it.svg",
+            logo: "https://media.api-sports.io/football/leagues/135.png",
+            fixtures: [],
+        },
+        {
+            league_name: "Bundesliga",
+            league_id: 2755,
+            flag: "https://media.api-sports.io/flags/de.svg",
+            logo: "https://media.api-sports.io/football/leagues/78.png",
+            fixtures: [],
+        },
+    ]
