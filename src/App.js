624586f580a04ca09a99616084f513ae445f6626
brunommbruno <brunommbruno@gmail.com>
2021-01-23 02:42:16 +0000

    added route to single fixture

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import{
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import News from "./Components/News";
+import Fixture from "./Components/Fixture";
 
 class App extends Component{
 
@@ -23,6 +24,10 @@ class App extends Component{
           <Route exact path="/news">
             <News />
           </Route>
+          {/* Takes Fixture Id and displays match */}
+          <Route exact path="/fixtures/:id" render={ ({match}) => (
+            <Fixture id={match.params.id} />
+          )} />   
         </Switch>
         </>
       </HashRouter>
