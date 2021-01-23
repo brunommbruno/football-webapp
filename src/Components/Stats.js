7886ff1a7c2767e8a3f062ed3f1c22d2cb2ea3ae
brunommbruno <brunommbruno@gmail.com>
2021-01-23 17:46:12 +0000

    added array to loop through statistics

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -6,107 +6,52 @@ class Stats extends Component{
     render(){
         const { statistics } = this.props;
 
+        //Array so possible to loop through specified statistics
+        let ordered = [
+            ["Possession", statistics["Ball Possession"]],
+            ["Total Shots",statistics["Total Shots"]],
+            ["On Target",statistics["Shots on Goal"]],
+            ["Total Passes",statistics["Total passes"]],
+            ["Corners",statistics["Corner Kicks"]],
+            ["Fouls",statistics["Fouls"]],
+            ["Offsides",statistics["Offsides"]],
+            ["Yellow Cards",statistics["Yellow Cards"]],
+            ["Red Cards",statistics["Red Cards"]]
+        ]
+
+        console.log(statistics)
+
         return(
             <Container>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Ball Possession"].home}</Badge>
-                        </Col>
-                        <Col><p>Possession</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Ball Possession"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Total Shots"].home}</Badge>
-                        </Col>
-                        <Col><p>Total Shots</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Total Shots"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Shots on Goal"].home}</Badge>
-                        </Col>
-                        <Col><p>On Target</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Shots on Goal"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Total passes"].home}</Badge>
-                        </Col>
-                        <Col><p>Total Passes</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Total passes"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Corner Kicks"].home}</Badge>
-                        </Col>
-                        <Col><p>Corners</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Corner Kicks"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Fouls"].home}</Badge>
-                        </Col>
-                        <Col><p>Fouls</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Fouls"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Offsides"].home}</Badge>
-                        </Col>
-                        <Col><p>Offsides</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Offsides"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Yellow Cards"].home}</Badge>
-                        </Col>
-                        <Col><p>Yellow Cards</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Yellow Cards"].away}</Badge>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <Badge>{statistics["Red Cards"].home}</Badge>
-                        </Col>
-                        <Col><p>Red Cards</p>
-                        </Col>
-                        <Col>
-                        <Badge>{statistics["Red Cards"].away}</Badge>
-                        </Col>
-                    </Row>
+                {ordered.map((stat) => (
+
+                <Row>
+                    <Col>
+                        <Badge>
+                            {stat[1].home === null ? 
+                            "N/A"
+                            :
+                            stat[1].home
+                            }
+                        </Badge>
+                    </Col>
+                    <Col>
+                        <p>{stat[0]}</p>
+                    </Col>
+                    <Col>
+                        <Badge>
+                            {stat[1].away === null ? 
+                            "N/A"
+                            :
+                            stat[1].away
+                            }
+                        </Badge>
+                    </Col>
+                </Row>
+
+                ))}
+
                     <Row style={{marginTop: "3rem"}}>
-                        <Col>
-                        <Badge></Badge>
-                        </Col>
-                        <Col>
-                        </Col>
-                        <Col>
-                        <Badge></Badge>
-                        </Col>
                     </Row>
             </Container>
         )
