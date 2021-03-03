97c5fed7e870c5710feb6b54a5a71846186e95e2
brunommbruno <brunommbruno@gmail.com>
2021-03-03 22:15:04 +0000

    removed navbar and news route

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import { Component } from "react";
-import{
-  HashRouter,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import News from "./Components/News";
 import Fixture from "./Components/Fixture";
 
-class App extends Component{
-
-  render(){
-    return(
-      <HashRouter basename='/'>
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
         <>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/news">
-            <News />
-          </Route>
-          {/* Takes Fixture Id and displays match */}
-          <Route exact path="/fixtures/:id" render={ ({match}) => (
-            <Fixture id={match.params.id} />
-          )} />   
-        </Switch>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* Takes Fixture Id and displays match */}
+            <Route
+              exact
+              path="/fixtures/:id"
+              render={({ match }) => <Fixture id={match.params.id} />}
+            />
+          </Switch>
         </>
       </HashRouter>
-    )
+    );
   }
-
-
 }
 
 export default App;
