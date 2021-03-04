dd4e0746ee07cc24aa274f1a87c6c5dff337558c
brunommbruno <brunommbruno@gmail.com>
2021-03-04 16:23:14 +0000

    removed useless hamburger icon in navbar and changed bg color to grey

--- src/Components/Fixture.js
+++ src/Components/Fixture.js
@@ -1,147 +1,141 @@
 import { Component } from "react";
 import axios from "../axios";
 import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
 
-import {  Row, Col, Badge } from "react-bootstrap";
+import { Row, Col, Badge } from "react-bootstrap";
 import LoadingAnim from "./LoadingAnim";
 import FixtureTab from "./FixtureTab";
 import Stats from "./Stats";
 import Lineups from "./Lineups";
 import MatchFacts from "./MatchFacts";
-import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 
-class Fixture extends Component{
+class Fixture extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      fixture: [],
+      loaded: false,
+      menuSelected: "match_facts",
+    };
 
-        this.state = {
-            fixture: [],
-            loaded: false,
-            menuSelected: "match_facts",
-        }
+    this.getRequest = this.getRequest.bind(this);
+    this.changeMenuSelection = this.changeMenuSelection.bind(this);
+  }
 
-        this.getRequest = this.getRequest.bind(this);
-        this.changeMenuSelection = this.changeMenuSelection.bind(this);
-    }
+  getRequest() {
+    axios.get(`/fixtures/id/${this.props.id}`).then(({ data }) => {
+      this.setState({ fixture: data.api.fixtures[0], loaded: true });
+    });
+  }
 
-    getRequest(){
-        axios.get(`/fixtures/id/${this.props.id}`).then(({data}) => {
-            this.setState({fixture:data.api.fixtures[0], loaded: true})
-        })
-    }
+  changeMenuSelection(menu) {
+    this.setState({ menuSelected: menu });
+  }
 
-    changeMenuSelection(menu){
-        this.setState({menuSelected: menu});
-    }
+  componentDidMount() {
+    this.getRequest();
+  }
 
-    componentDidMount(){
-        this.getRequest();
-    }
+  render() {
+    const { fixture, loaded, menuSelected } = this.state;
 
-    render(){
+    console.log(fixture);
 
-        const { fixture, loaded, menuSelected } = this.state;
+    return (
+      <div className="fixture">
+        <AppBar position="static" className="appbar">
+          <Toolbar>
+            <KeyboardBackspaceIcon
+              onClick={() => window.history.back()}
+              className="mr-3"
+              style={{ fontSize: "2rem" }}
+            />
+            <Typography variant="h6">Match</Typography>
+          </Toolbar>
+        </AppBar>
 
-        console.log(fixture)
+        {!loaded ? (
+          <LoadingAnim />
+        ) : (
+          <>
+            <Row className="score">
+              <Col className="home-team">
+                <img src={fixture.homeTeam.logo} alt="home team logo" />
+                <p>{fixture.homeTeam.team_name}</p>
+              </Col>
+              <Col>
+                <p className="live-score">
+                  {fixture.statusShort === "NS"
+                    ? `${fixture.event_date.slice(11, 16)}`
+                    : fixture.goalsHomeTeam === null
+                    ? `0 - 0`
+                    : `${fixture.goalsHomeTeam} - ${fixture.goalsAwayTeam}`}
+                </p>
+                <p>
+                  {fixture.statusShort === "FT" ? (
+                    "Full Time"
+                  ) : fixture.statusShort === "HT" ? (
+                    "Half Time"
+                  ) : fixture.statusShort === "NS" ? (
+                    "Not Started"
+                  ) : (
+                    <Badge className="blink text-white">
+                      {fixture.elapsed}
+                    </Badge>
+                  )}
+                </p>
+              </Col>
+              <Col className="away-team">
+                <img src={fixture.awayTeam.logo} alt="away team logo" />
+                <p>{fixture.awayTeam.team_name}</p>
+              </Col>
+            </Row>
 
-        return(
-            <div className="fixture">
+            <FixtureTab
+              changeMenuSelection={this.changeMenuSelection}
+              menuSelected={menuSelected}
+            />
 
-                <AppBar position="static" className="appbar">
-                    <Toolbar>
-                        <KeyboardBackspaceIcon 
-                            onClick={() => window.history.back()}
-                            className="mr-3"
-                            style={{fontSize:"2rem"}}
-                        />
-                        <Typography variant="h6">
-                            Match
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-
-            {!loaded ? <LoadingAnim /> : 
-                <>
-                <Row className="score">
-                    <Col className="home-team">
-                        <img src={fixture.homeTeam.logo} alt="home team logo"/>
-                        <p>{fixture.homeTeam.team_name}</p>
-                    </Col>
-                    <Col>
-                        <p className="live-score">
-                            {fixture.statusShort === "NS" ? 
-                            `${fixture.event_date.slice(11, 16)}`
-                            :
-                            fixture.goalsHomeTeam === null ?
-                            `0 - 0`
-                            :
-                            `${fixture.goalsHomeTeam} - ${fixture.goalsAwayTeam}`
-                            }
-                        </p>
-                        <p>{fixture.statusShort === "FT" ? "Full Time" :
-                            fixture.statusShort === "HT" ? "Half Time" :
-                            fixture.statusShort === "NS" ? "Not Started" :
-                            <Badge className="blink text-white">{fixture.elapsed}</Badge>
-                        }</p>
-                    </Col>
-                    <Col className="away-team">
-                        <img src={fixture.awayTeam.logo} alt="away team logo" />
-                        <p>{fixture.awayTeam.team_name}</p>
-                    </Col>
-                </Row>
-                
-                <FixtureTab 
-                    changeMenuSelection={this.changeMenuSelection} 
-                    menuSelected={menuSelected}
-                />
-
-                {menuSelected === "match_facts" ?
-                <MatchFacts 
-                    events={fixture.events}
-                    referee={fixture.referee}
-                    venue={fixture.venue}
-                    teamOne={fixture.homeTeam.team_name}
-                    teamTwo={fixture.awayTeam.team_name}
-                />
-                :
-                menuSelected === "stats" ?
-                <div className="stats">
-                    {/* Stats component will break if statistics are not available yet */}
-                    {fixture.statistics ?
-                    <Stats 
-                        statistics={fixture.statistics}
+            {menuSelected === "match_facts" ? (
+              <MatchFacts
+                events={fixture.events}
+                referee={fixture.referee}
+                venue={fixture.venue}
+                teamOne={fixture.homeTeam.team_name}
+                teamTwo={fixture.awayTeam.team_name}
+              />
+            ) : menuSelected === "stats" ? (
+              <div className="stats">
+                {/* Stats component will break if statistics are not available yet */}
+                {fixture.statistics ? (
+                  <Stats statistics={fixture.statistics} />
+                ) : (
+                  <p>No Statistics Available Yet</p>
+                )}
+              </div>
+            ) : menuSelected === "lineup" ? (
+              <>
+                {fixture.lineups ? (
+                  <div className="lineup">
+                    <Lineups
+                      lineups={fixture.lineups}
+                      teamOne={fixture.homeTeam.team_name}
+                      teamTwo={fixture.awayTeam.team_name}
                     />
-                    :
-                    <p>No Statistics Available Yet</p>
-                    }
-                </div>
-                :
-                menuSelected === "lineup" ?
-                <>
-                    {fixture.lineups ? 
-                        <div className="lineup">
-                    <Lineups 
-                        lineups={fixture.lineups}
-                        teamOne={fixture.homeTeam.team_name}
-                        teamTwo={fixture.awayTeam.team_name}
-                    />
-                    </div>
-                    :
-                    <p className="text-center mt-4">No Lineup Available Yet</p>    
-                    }
-                </>
-                :
-                null
-            }
-                </>
-            }
-
-            </div>
-        )
-    }
-
+                  </div>
+                ) : (
+                  <p className="text-center mt-4">No Lineup Available Yet</p>
+                )}
+              </>
+            ) : null}
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Fixture;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,77 +1,70 @@
 import { Component } from "react";
 import axios from "../axios";
 import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
 import DayTab from "./DayTab";
 import HomeFixtures from "./HomeFixtures";
 import LoadingAnim from "./LoadingAnim";
 
-class Home extends Component{
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    const day = new Date();
+    const today = day.toISOString().slice(0, 10);
+    const hour = day.getHours();
+    const min = day.getMinutes();
 
-        const day = new Date();
-        const today = day.toISOString().slice(0, 10)
-        const hour = day.getHours();
-        const min = day.getMinutes();
+    this.state = {
+      fixtures: [],
+      currentDate: `${today}`,
+      currentTime: `${hour}:${min}`,
+      loaded: false,
+    };
 
-        this.state = {
-            fixtures: [],
-            currentDate: `${today}`,
-            currentTime: `${hour}:${min}`,
-            loaded: false,
-        }
+    this.getRequest = this.getRequest.bind(this);
+    this.changeDate = this.changeDate.bind(this);
+  }
 
-        this.getRequest = this.getRequest.bind(this);
-        this.changeDate = this.changeDate.bind(this);
-    }
+  getRequest(date) {
+    this.setState({ loaded: false });
+    axios.get(`/fixtures/date/${date}`).then(({ data }) => {
+      this.setState({ fixtures: data.api.fixtures, loaded: true });
+    });
+  }
 
-     getRequest(date){
-         this.setState({loaded: false})
-         axios.get(`/fixtures/date/${date}`).then(({data}) => {
-             this.setState({fixtures: data.api.fixtures, loaded: true})
-         })
-     }
+  changeDate(date) {
+    this.getRequest(date);
+  }
 
-     changeDate(date){
-         this.getRequest(date);
-     }
+  componentDidMount() {
+    this.getRequest(this.state.currentDate);
+  }
 
-     componentDidMount(){
-         this.getRequest(this.state.currentDate);
-     }
+  render() {
+    return (
+      <div className="home">
+        <AppBar position="static" className="appbar">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            ></IconButton>
+            <Typography variant="h6">Home</Typography>
+          </Toolbar>
+        </AppBar>
 
-    render(){
-
-        return(
-            <div className="home">
-
-                <AppBar position="static" className="appbar">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6">
-                        Matches
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-
-                <DayTab 
-                    changeDate={this.changeDate}
-                />
-                {/* Fixtures component will only load once get request has been sucessful */}
-                {this.state.loaded ? 
-                <HomeFixtures 
-                    fixtures={this.state.fixtures}
-                />
-                :
-                <LoadingAnim />
-                }
-            </div>
-        )
-    }
+        <DayTab changeDate={this.changeDate} />
+        {/* Fixtures component will only load once get request has been sucessful */}
+        {this.state.loaded ? (
+          <HomeFixtures fixtures={this.state.fixtures} />
+        ) : (
+          <LoadingAnim />
+        )}
+      </div>
+    );
+  }
 }
 
 export default Home;
